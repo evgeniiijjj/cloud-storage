73892b5e97ef3d19972191ef0e3f4d92ae78ef29
evgeniiijjj <100117060+evgeniiijjj@users.noreply.github.com>
2022-12-22 16:30:05 +0300

    Update JwtBlacklistRepository.java

--- src/main/java/com/example/cloudstorage/repositories/JwtBlacklistRepository.java
+++ src/main/java/com/example/cloudstorage/repositories/JwtBlacklistRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JwtBlacklistRepository extends JpaRepository<JwtBlacklist, Long> {
+public interface JwtBlacklistRepository extends JpaRepository<JwtBlacklist, String> {
     boolean existsByJwt(String jwt);
 }
